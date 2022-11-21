6058a6b728b81d8c87946fcde6c14564599ca8e8
NastyDrac <116013956+NastyDrac@users.noreply.github.com>
2022-11-21 13:01:45 -0500

    dem eggs is bangin'!
    egg have become gangster

--- Egg.gd
+++ Egg.gd
@@ -1,12 +1,17 @@
 extends Area2D
 
-
+onready var timer = get_node("Timer")
 onready var map = get_parent().get_parent()
-
-
+onready var animation = get_node("AnimatedSprite")
+func _ready():
+	timer.start(rand_range(8, 10))
 
 func _on_Timer_timeout():
+	animation.playing = true
+
+
+
+func _on_AnimatedSprite_animation_finished():
 	var hatch_pos = Vector2(position.x + rand_range(-10, 10), position.y + rand_range(-10, 10))
 	map.spawn_bird(hatch_pos)
 	queue_free()
-
